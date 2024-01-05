acb9c817ba75ad933adea885cc9b94bded7b3083
Chenggang Wang <cg.tennis@gmail.com>
2024-01-04 19:06:24 -0500

    Generics Parameter Types reflection

---
+++ src/generic/Display.java
@@ -0,0 +1,15 @@
+package generic;
+
+import java.util.List;
+
+public class Display
+{
+
+    protected List<Long> nameList;
+
+    public void setNameList(List<Long> nameList)
+    {
+        this.nameList = nameList;
+    }
+
+}
